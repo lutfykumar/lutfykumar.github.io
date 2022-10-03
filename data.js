da3dd3def6f051e714a6db9522f09785f1c26160
lutfykumar <lutfhiesita@gmail.com>
2022-10-03 23:10:55 +0700

    deploy github

---
+++ data.js
@@ -0,0 +1,47 @@
+const produks = [
+  {
+    name: "Web ERP",
+    author: "Bangchan",
+    images: [
+      {
+        thumb: "https://lipsum.app/id/3/200x150",
+        src: "https://lipsum.app/id/3/800x600",
+        caption: "Web ERP"
+      }, {
+        thumb: "https://lipsum.app/id/3/200x150",
+        src: "https://lipsum.app/id/3/800x600",
+        caption: "Web ERP"
+      }, {
+        thumb: "https://lipsum.app/id/3/200x150",
+        src: "https://lipsum.app/id/3/800x600",
+        caption: "Web ERP"
+      }, {
+        thumb: "https://lipsum.app/id/3/200x150",
+        src: "https://lipsum.app/id/3/800x600",
+        caption: "Web ERP"
+      }
+    ]
+  }, {
+    name: "Landing Page",
+    author: "Bangchan",
+    images: [
+      {
+        thumb: "https://lipsum.app/id/3/200x150",
+        src: "https://lipsum.app/id/3/800x600",
+        caption: "Landing Page - Front"
+      }, {
+        thumb: "https://lipsum.app/id/3/200x150",
+        src: "https://lipsum.app/id/3/800x600",
+        caption: "Landing Page - Mobile"
+      }, {
+        thumb: "https://lipsum.app/id/3/200x150",
+        src: "https://lipsum.app/id/3/800x600",
+        caption: "Landing Page - Example"
+      }, {
+        thumb: "https://lipsum.app/id/3/200x150",
+        src: "https://lipsum.app/id/3/800x600",
+        caption: "Landing Page - Static"
+      }
+    ]
+  }
+]
